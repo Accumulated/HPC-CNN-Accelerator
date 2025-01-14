aab7009e5df8427b91ec37d45a08ba4dc1508f0c
Accumulated <muhammedmahmoud063@gmail.com>
2025-01-15 00:41:20 +0100

    Add initial Project structure

---
+++ Accelerator/1_Software/Application/APP.cu
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include "device_launch_parameters.h"
+#include <cusolverDn.h>
+#include <cuda_runtime.h>
+
+
+int MBCONV1_0_flag = 0;
+
+int main()
+{
+
+}
